c9a7e0608c8dffa24a65ca2fdda87af446a5fed1
fangt11 <fangt11@ke.com>
2020-11-21 14:48:08 +0800

    fix: 静态资源路径

--- deploy.js
+++ deploy.js
@@ -5,4 +5,5 @@ const htmlContent = readFileSync('./build/index.html').toString();
 
 const githubPagesStaticPrefix = ['https://shuoshubao.github.io', name, 'build/static'].join('/');
 
-writeFileSync('./index.html', htmlContent.replace(/\/static/g, githubPagesStaticPrefix) + '\n');
+// writeFileSync('./index.html', htmlContent.replace(/\/static/g, githubPagesStaticPrefix) + '\n');
+writeFileSync('./index.html', htmlContent + '\n');
